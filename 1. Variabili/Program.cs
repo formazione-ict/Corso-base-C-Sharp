7829b0261bef1d319531488f07e5deae162c31b5
Marino Speciale <149328783+formazione-ict@users.noreply.github.com>
2024-10-01 14:16:14 +0200

    Add files via upload

---
+++ 1. Variabili/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _1._Variabili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //Type nomevariabile = Value;
+            //string nome = "";
+            string nome = "Mario";
+            string telefono = "3334589763";
+            Console.WriteLine(nome);
+            Console.WriteLine(telefono);
+            Console.WriteLine("Ciao " + nome); Console.WriteLine("Il mio numero di telefono è " + telefono);
+
+            string cognome = "Rossi";
+            string nominativo = nome + cognome;
+            Console.WriteLine("Benvenuto " + nominativo);
+
+            int numero = 10;
+            Console.WriteLine(numero);
+           
+            double decimale = 10.533;
+            Console.WriteLine(decimale);
+
+            char lettera = 'S';
+            Console.WriteLine(lettera);
+
+            bool booleano = false;
+
+            int a = 10;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine("La somma di a e b è " + c); //15
+
+            string a1 = "10";
+            string b1 = "5";
+            string c1 = a1 + b1;
+            Console.WriteLine(c1); //105
+
+            int a2 = 10, b2 = 20, c2 = 30; //60
+        }
+    }
+}
---
+++ 3. Type Casting/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Type_Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type Casting in C#
+            string nome = "Mario";
+
+            //Implicit Casting (automatico)
+            //char -> int -> long -> float -> double
+
+
+            //Explicit Casting (manuale)
+            //double -> float -> long -> int -> char
+
+            int myInt = 8;
+            double myDouble = myInt;
+
+            Console.WriteLine(myDouble);
+
+            double mydouble = 9.9;
+            //int myint = mydouble; non è possibile convertire
+            int myint = (int)mydouble;
+
+            Console.WriteLine(myint);
+
+            //Metodo di conversione di tipo
+            //Convert.To...
+          
+            int myInt2 = 9;
+            double myDouble2 = 20.77;
+
+            Console.WriteLine(Convert.ToDecimal(myInt2)); //non è possibile
+            Console.WriteLine(Convert.ToInt32(myDouble2));
+
+        }
+    }
+}
