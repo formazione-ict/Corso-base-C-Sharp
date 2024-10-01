5f096848e65ae3ea47bf76bccd685892873cc481
Marino Speciale <149328783+formazione-ict@users.noreply.github.com>
2024-10-01 14:14:48 +0200

    Add files via upload

---
+++ 2. Identificatori/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Identificatori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Identificatori in C#
+            string nome = "Mario Rossi";
+
+            // _ (Underscore)
+            string codice_fiscale = "";
+
+            // (A-Z, (a-z), 0-9 e _)
+            string mioNome = "Mario"; 
+
+            Console.WriteLine(mioNome); //Console.WriteLine(mionome); ERRORE
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
