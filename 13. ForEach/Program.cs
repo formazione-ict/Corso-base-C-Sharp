89ee653a10191d8f39e80b0b3de0428196d17b3c
Marino Speciale <149328783+formazione-ict@users.noreply.github.com>
2024-10-01 14:47:10 +0200

    Add files via upload

---
+++ 13. ForEach/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prima parte Dichiarazione For Each
+            /*Il ciclo foreach in C# permette di enumerare gli elementi di una collezione, eseguendo un'azione per ogni elemento
+             senza la necessità di gestire manualmente gli indici.
+             La sintassi del ciclo foreach è semplice: foreach (tipo elemento in collezione) { // codice da eseguire }, 
+             dove il 'tipo' rappresenta il tipo degli elementi nella collezione.
+             Utilizzando il ciclo foreach si aumenta la leggibilità del codice e si riduce il rischio di errori, poiché non è necessario
+             gestire gli indici degli array o delle liste. */
+
+            string[] viaggio = { "Automobile", "Treno", "Bus", "Aereo", "Moto", "Nave" };
+
+            foreach (string i in viaggio)
+            {
+
+                // Dichiarazione
+
+                Console.WriteLine("Il tuo valore è: " + i);
+
+
+                // Seconda parte Break e Continue
+                if (i == "Bus")
+                {
+                    // Dichiarazione semplice
+
+                    break; // Puoi sostituire con continue
+                }
+            }
+        }
+    }
+}
---
+++ 15. Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays
+            /* Gli arrays (matrici o vettori) indicano una struttura di dati formata da più componenti ordinati dello stesso tipo
+               e identificate ognuna da uno o più indici che ne determinano univocamente la posizione*/
+
+            string[] viaggio = { "Automobile", "Treno", "Bus", "Aereo", "Moto", "Nave" };
+            int[] myNum = { 10, 5, 20, 1, 30, 7 };
+
+            //viaggio[0] = "Nessun mezzo di trasporto";
+
+            Console.WriteLine(viaggio[0]);
+
+            Console.WriteLine(myNum[0]);
+
+            Console.WriteLine("Gli elementi presenti nel mio array sono: " + viaggio.Length); // Per conoscere il numero degli elementi presenti nel vettore
+
+            // Arrays usando il ciclo for per visualizzare tutti gli elementi presenti
+
+            for (int i = 0; i < viaggio.Length; i++)
+            {
+                Console.WriteLine(viaggio[i]);
+                Console.WriteLine("\t" );
+            }
+
+            // Arrays usando il ciclo foreach per visualizzare solo gli elementi che desidero
+            // La procedura Sort effettua l'ordinamento degli elementi contenuti nell'array
+           
+            Array.Sort(viaggio);
+
+            foreach (string i in viaggio)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Possiamo provarlo anche con myNum
+
+            Array.Sort(myNum);
+
+            foreach (int i in myNum)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
